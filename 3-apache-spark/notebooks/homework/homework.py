22de82b8fae26953ae7ac25a76c30401179e6cd5
Meor Hasyim <40785172+meoflaxz@users.noreply.github.com>
2025-01-16 23:59:39 +0800

    update

--- 3-apache-spark/notebooks/homework/homework.py
+++ 3-apache-spark/notebooks/homework/homework.py
@@ -16,9 +16,6 @@ spark = (SparkSession.builder
         .getOrCreate())
 
 
-from IPython.core.display import HTML
-display(HTML("<style>pre { white-space: pre !important; }</style>"))
-
 # import matches
 matches_bucketed = spark.read.option("header", "true") \
                 .option("inferSchema", "true") \
@@ -37,7 +34,12 @@ medals_matches_players_bucketed = spark.read.option("header", "true") \
 # import medals
 medals_bucketed = spark.read.option("header", "true") \
                 .option("inferSchema", "true") \
-                .csv("/home/iceberg/data/medals.csv") 
+                .csv("/home/iceberg/data/medals.csv")
+
+# import maps
+maps_bucketed = spark.read.option("header", "true") \
+                .option("inferSchema", "true") \
+                .csv("/home/iceberg/data/maps.csv") 
 
 # disabling broadcast join threshold
 spark.conf.set("spark.sql.autoBroadcastJoinThreshold", "-1")
@@ -120,4 +122,122 @@ for row in distinctPlayer_gamertag.collect():
 
 # Verify the data in the table
 result = spark.sql("SELECT * FROM bootcamp.match_details_bucketed")
+result.show()
+
+medals_matches_players_bucketedDDL = """CREATE TABLE IF NOT EXISTS bootcamp.medals_matches_players_bucketed (
+                         match_id STRING,
+                         player_gamertag STRING,
+                         match_id INTEGER,
+                         count INTEGER
+                     )
+                     USING iceberg
+                     PARTITIONED BY (player_gamertag, bucket(16, match_id));
+                     """
+spark.sql(medals_matches_players_bucketedDDL)
+
+# Get distinct player_gamertag
+distinctPlayer_gamertag = medals_matches_players_bucketed.select("player_gamertag").distinct()
+
+# Process data in chunks based on completion_date
+for row in distinctPlayer_gamertag.collect():
+    gamertag  = row["player_gamertag"]
+    filteredMatches = medals_matches_players_bucketed.filter(col("player_gamertag") == gamertag )
+    
+    # Repartition and persist the filtered data
+    optimizedMatches = filteredMatches \
+        .select("match_id", "player_gamertag", "match_id", "count") \
+        .repartition(16, "match_id") \
+        .persist(StorageLevel.MEMORY_AND_DISK)
+    
+    # Write the data to the table
+    optimizedMatches.write \
+        .mode("append") \
+        .bucketBy(16, "match_id") \
+        .partitionBy("player_gamertag") \
+        .saveAsTable("bootcamp.medals_matches_players_bucketed")
+    
+    # Unpersist the DataFrame to free up memory
+    optimizedMatches.unpersist()
+
+# Verify the data in the table
+result = spark.sql("SELECT * FROM bootcamp.medals_matches_players_bucketed")
+result.show()
+
+medalsDDL = """CREATE TABLE IF NOT EXISTS bootcamp.medals_bucketed (
+                         medal_id STRING,
+                         name STRING,
+                         difficulty INTEGER
+                     )
+                     USING iceberg
+                     PARTITIONED BY (name, bucket(16, medal_id));
+                     """
+spark.sql(medalsDDL)
+
+# Get distinct medal
+distinctMedal_name = medals_bucketed.select("name").distinct()
+
+# Process data in chunks based on completion_date
+for row in distinctMedal_name.collect():
+    medal  = row["name"]
+    filteredMatches = medals_matches_players_bucketed.filter(col("name") == medal )
+    
+    # Repartition and persist the filtered data
+    optimizedMatches = filteredMatches \
+        .select("medal_id", "name", "difficulty") \
+        .repartition(16, "medal_id") \
+        .persist(StorageLevel.MEMORY_AND_DISK)
+    
+    # Write the data to the table
+    optimizedMatches.write \
+        .mode("append") \
+        .bucketBy(16, "medal_id") \
+        .partitionBy("name") \
+        .saveAsTable("bootcamp.medals_bucketed")
+    
+    # Unpersist the DataFrame to free up memory
+    optimizedMatches.unpersist()
+
+# Verify the data in the table
+result = spark.sql("SELECT * FROM bootcamp.medals_bucketed")
+result.show()
+
+## BROADCAST JOIN FOR MEDALS AND MAPS
+spark.conf.set("spark.sql.autoBroadcastJoinThreshold", "1000000000000")
+
+
+maps_bucketedDDL = """CREATE TABLE IF NOT EXISTS bootcamp.maps_bucketed (
+                         mapid STRING,
+                         name STRING
+                     )
+                     USING iceberg
+                     PARTITIONED BY (name, bucket(16, mapid));
+                     """
+spark.sql(maps_bucketedDDL)
+
+# Get distinct map
+distinctMap_name = maps_bucketed.select("name").distinct()
+
+# Process data in chunks based on completion_date
+for row in distinctMap_name.collect():
+    medal  = row["name"]
+    filteredMatches = maps_bucketed.filter(col("name") == medal )
+    
+    # Repartition and persist the filtered data
+    optimizedMatches = filteredMatches \
+        .select("mapid", "name") \
+        .repartition(16, "mapid") \
+        .persist(StorageLevel.MEMORY_AND_DISK)
+    
+    # Write the data to the table
+    optimizedMatches.write \
+        .mode("append") \
+        .bucketBy(16, "medal_id") \
+        .partitionBy("name") \
+        .saveAsTable("bootcamp.maps_bucketed")
+    
+    # Unpersist the DataFrame to free up memory
+    optimizedMatches.unpersist()
+
+# Verify the data in the table
+result = spark.sql("SELECT * FROM bootcamp.maps_bucketed")
 result.show()
